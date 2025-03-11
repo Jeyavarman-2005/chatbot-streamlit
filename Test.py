44c48eed00ad2a7eb30bbbf691f432bc08127172
Jeyavarman <jeyavarmamm.mech2022@citchennai.net>
2025-03-11 21:47:31 +0530

    Delete Test.py

--- Test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Retrieve values from environment variables
-api_key = os.getenv("API_KEY")
-google_credentials = os.getenv("GOOGLE_CREDENTIALS")
-
-# Print or use them in your script
-print("API Key:", api_key)
-print("Google Credentials File:", google_credentials)
